dff2effdfa1941f8ef98d99afe13493329fca518
Harry Pidd <hjpidd@gmail.com>
2020-09-19 15:09:54 +1000

    change db

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys').mongoURI;
+const db = require('config/MONGODB_URI');
 
 // Connect to MongoDB
 mongoose
