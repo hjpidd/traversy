6ece42de486501505fcaeb23d9e0d6a64eb94dae
Harry Pidd <hjpidd@gmail.com>
2020-09-19 15:20:03 +1000

    updated port

--- server.js
+++ server.js
@@ -34,4 +34,6 @@ if(process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
