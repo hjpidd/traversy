abd0fe0f216ef8e533b14356905fbeb94692576a
Harry Pidd <hjpidd@gmail.com>
2020-09-24 15:18:00 +1000

    hi again

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -30,7 +30,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <RegisterModal />
+                                    <a href="hi.html">Hi</a>
                                 </NavItem>
                             </Nav>
                         </Collapse>
